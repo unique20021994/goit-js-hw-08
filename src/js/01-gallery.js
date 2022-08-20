bb32e5970485b76f20dc8191c89f473faf415f77
unique20021994 <unique20021994@gmail.com>
2022-08-20 17:54:00 +0300

    done 1st task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,29 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector('.gallery');
+const items = [];
+
+galleryItems.forEach(element => {
+  const galleryItem = document.createElement('li');
+  const galleryLink = document.createElement('a');
+  galleryLink.className = 'gallery__item';
+  galleryLink.href = element.original;
+  const galleryImage = document.createElement('img');
+  galleryImage.className = 'gallery__image';
+  galleryImage.src = element.preview;
+  galleryImage.setAttribute('title', element.description);
+  galleryImage.alt = element.description;
+  galleryItem.append(galleryLink);
+  galleryLink.append(galleryImage);
+  items.push(galleryItem);
+})
+gallery.append(...items);
+new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+})
